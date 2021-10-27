9c128bffcd9d5e4aa488a9d1df6cd01b1f992d9a
mks <gemuyong241@gamil.com>
2021-10-27 17:07:56 +0900

    Add_admin

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,7 +30,7 @@ class TasksController < ApplicationController
   def create
     @task=Task.new(task_params)
     if @task.save
-      redirect_to @tasks,notice:"タスク「#{@task.name}」を登録しました。"
+      redirect_to @task,notice:"タスク「#{@task.name}」を登録しました。"
     else
       render:new
     end
